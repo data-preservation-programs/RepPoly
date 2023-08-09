84059eb3364d1a06043a85db98d074a405a50150
stephen-pl <139834872+stephen-pl@users.noreply.github.com>
2023-08-08 23:19:32 -0700

    Updating Run Script

--- src/run.ts
+++ src/run.ts
@@ -20,6 +20,8 @@ const polydb = new Polybase({
 
 const mongo = new MongoClient(process.env.MONGO_URI!)
 const queue = new PQueue({concurrency: 64})
+const schemasNeedingDateStamp = ['filfox', 'filscan', 'gravity_assist_retrieval_bot', 'ground_control_sp_location', 
+'lassie_bedrock', 'protocol_labs_retrieval_bot', 'filecoin_foundation_retrieval_bot', 'slingshot_retrievalbot', 'starboard'];
 
 try {
     for (const {repdao, polybase} of collections) {
@@ -34,28 +36,63 @@ try {
                 $gt: new ObjectId(lastId)
             }
         }
+
         for await (const doc of mongoCol.find(match).sort('_id', 'asc')) {
             queue.add(() => {
                 console.log(`Creating ${polybase} record for`, doc._id.toString())
-                const fields = argFields[repdao]
-                const values = fields.map((field) => {
-                    const value = doc[field.name]
-                    if (value instanceof ObjectId) {
-                        return value.toString()
+                var fields = argFields[repdao];
+                if (schemasNeedingDateStamp.includes(polybase)) {
+                    fields.push({name:'ds', type:'string'})
+                }
+                fields.sort(function(a, b) {
+                    if (a.name == '_id') {
+                        return 1;
                     }
-                    if (value == null) {
-                        switch (field.type) {
-                            case 'string':
-                                return ''
-                            case 'number':
-                                return 0
-                            case 'boolean':
-                                return false
-                            default:
-                                throw new Error(`Unknown type ${field.type}`)
+                    if (b.name == '_id') {
+                        return 1;
+                    }
+                    return a.name.localeCompare(b.name);
+                  });
+
+                const values = fields.map((field) => {
+                    console.log(field.name + "   " + doc[field.name])
+                    if (field.name == 'ds') {
+                        if (polybase == 'filfox' || polybase == 'filscan') {
+                            return epochToDate(doc['epoch'])
+                        }
+                        else if (polybase == 'protocol_labs_retrieval_bot' || polybase == 'gravity_assist_retrieval_bot'
+                        || polybase == 'slingshot_retrievalbot' || polybase == 'filecoin_foundation_retrieval_bot') {
+                            return doc['date']
+                        }
+                        else if (polybase == 'ground_control_sp_location') {
+                            return doc['date'].toISOString().substring(0, 10)
+                        }
+                        else if (polybase == 'lassie_bedrock') {
+                            return doc['start_time'].toISOString().substring(0, 10)
+                        }
+                        else if (polybase == 'starboard') {
+                            return doc['stat_date'].substring(0, 10)
                         }
                     }
-                    return value
+                    else {
+                        const value = doc[field.name]
+                        if (value instanceof ObjectId) {
+                            return value.toString()
+                        }
+                        if (value == null) {
+                            switch (field.type) {
+                                case 'string':
+                                    return ''
+                                case 'number':
+                                    return 0
+                                case 'boolean':
+                                    return false
+                                default:
+                                    throw new Error(`Unknown type ${field.type}`)
+                            }
+                        }
+                        return value
+                    }
                 })
                 return pRetry(async () => {
                     try {
@@ -76,3 +113,13 @@ try {
 } finally {
     await mongo.close()
 }
+
+function epochToDate(epoch: number) {
+    if (epoch  == undefined) {
+        return null
+    }
+    var diff_ms =  epoch * 1000 * 30 // block time in seconds
+    var date = diff_ms + new Date('2020-08-24T22:00Z').getTime() // filecoin network start time
+    var converted_date = new Date(date)
+    return converted_date.toISOString().substring(0, 10)
+  }
