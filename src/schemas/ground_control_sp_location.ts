8a083f9ff545ca6df656c29108c14c6895d6261a
stephen-pl <139834872+stephen-pl@users.noreply.github.com>
2023-08-14 13:06:23 -0700

    Removing kentik, replacing with ground_control_sp_location and Adding ds Support with sorted fields
    Rename kentik and replacing it with ground_control_sp_location. Kentik schema was deleted and replaced with ground_control_sp_location.
    
    
    
    Updating gen Script To support DateStamp (ds)
    
    
    Updating gen script to support a new field called ds which stands for datestamp. This field will be used to make it easier to query by dates in the YYYY-MM-DD time stamp. The params and fields are also sorted in this pr with the exception of id which is considered the id for the collection.
    
    ghstack-source-id: d683a22d1d5c789d8c2e0c36689a21ba5606443e
    Pull Request resolved: https://github.com/data-preservation-programs/RepPoly/pull/10

--- src/schemas/ground_control_sp_location.ts
+++ src/schemas/ground_control_sp_location.ts
@@ -1,15 +1,16 @@
 // Auto-generated, do not modify
-export interface kentik {
+export interface ground_control_sp_location {
   id: string;
-  date: string;
-  multiaddr: string;
-  provider: string;
-  testId: string;
   __v: number;
   agentCity: string;
   agentCountry: string;
   agentLatitude: number;
   agentLongitude: number;
   agentRegion: string;
+  date: string;
+  ds: string;
   latencyMs: number;
+  multiaddr: string;
+  provider: string;
+  testId: string;
 }
