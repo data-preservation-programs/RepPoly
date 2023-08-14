c0da19beb63a055b144d2a12d33742e25342ce19
stephen-pl <139834872+stephen-pl@users.noreply.github.com>
2023-08-14 13:06:28 -0700

    Updating Run Script
    updating args to beat race condition
    
    ghstack-source-id: b87fdf1d1bc88247fe6ea5ca8d91bad52a01caad
    Pull Request resolved: https://github.com/data-preservation-programs/RepPoly/pull/11

--- src/run.ts
+++ src/run.ts
@@ -22,7 +22,8 @@ const mongo = new MongoClient(process.env.MONGO_URI!)
 const queue = new PQueue({concurrency: 64})
 
 try {
-    for (const {repdao, polybase} of collections) {
+    for (const {repdao, polybase, provider} of collections) {
+
         const polyCol = polydb.collection(polybase)
         const mongoCol = mongo.db('reputation').collection(repdao)
         const last = await polyCol.sort('id', 'desc').limit(1).get()
@@ -34,33 +35,83 @@ try {
                 $gt: new ObjectId(lastId)
             }
         }
+
         for await (const doc of mongoCol.find(match).sort('_id', 'asc')) {
             queue.add(() => {
                 console.log(`Creating ${polybase} record for`, doc._id.toString())
-                const fields = argFields[repdao]
-                const values = fields.map((field) => {
-                    const value = doc[field.name]
-                    if (value instanceof ObjectId) {
-                        return value.toString()
+                var fields = argFields[repdao];
+                fields.forEach(function(f) {
+                    if (f.name === provider) {
+                        f.name = 'provider'
                     }
-                    if (value == null) {
-                        switch (field.type) {
-                            case 'string':
-                                return ''
-                            case 'number':
-                                return 0
-                            case 'boolean':
-                                return false
-                            default:
-                                throw new Error(`Unknown type ${field.type}`)
+                })
+
+                fields.sort(function(a, b) {
+                    if (a.name == '_id') {
+                        return 1;
+                    }
+                    if (b.name == '_id') {
+                        return 1;
+                    }
+                    return a.name.localeCompare(b.name);
+                  });
+ 
+                const values = fields.map((field) => {
+                    if (field.name == 'ds') {
+                        if (polybase == 'filfox' || polybase == 'filscan') {
+                            return epochToDate(doc['epoch'])
+                        }
+                        else if (polybase == 'protocol_labs_retrieval_bot' || polybase == 'gravity_assist_retrieval_bot'
+                        || polybase == 'slingshot_retrievalbot' || polybase == 'filecoin_foundation_retrieval_bot') {
+                            return doc['date']
+                        }
+                        else if (polybase == 'ground_control_sp_location') {
+                            return doc['date'].toISOString().substring(0, 10)
+                        }
+                        else if (polybase == 'lassie_bedrock') {
+                            return doc['start_time'].toISOString().substring(0, 10)
+                        }
+                        else if (polybase == 'starboard') {
+                            return doc['onboarding_at'].substring(0, 10)
                         }
                     }
-                    return value
+                    else {
+                        const value = doc[field.name === 'provider' ? provider : field.name]
+
+                        if (value === undefined) {
+                            return undefined;
+                        }
+                        if (value instanceof ObjectId) {
+                            return value.toString()
+                        }
+                        if (value == null) {
+                            switch (field.type) {
+                                case 'string':
+                                    return ''
+                                case 'number':
+                                    return 0
+                                case 'boolean':
+                                    return false
+                                default:
+                                    throw new Error(`Unknown type ${field.type}`)
+                            }
+                        }
+                        return value
+                    }
                 })
                 return pRetry(async () => {
                     try {
-                        await polyCol.create(values)
+                        if (!values.includes(undefined)) {
+                            console.log(`Creating row in ${polybase} polybase...`)
+                            await polyCol.create(values)
+                        }
+                        else {
+                            console.log(`Did not create record for ${doc._id.toString()}. Values ${values.join()}`)
+                        }
                     } catch (e : any) {
+                        console.log(e.toString() + ' in ' + polybase)
+                        fields.forEach(function(f) {console.log(f.name + " " + f.type)})
+                        console.log(values.join(', '));
                         if (e instanceof PolybaseError && e.code === 'already-exists') {
                             return
                         }
@@ -76,3 +127,13 @@ try {
 } finally {
     await mongo.close()
 }
+
+function epochToDate(epoch: number) {
+    if (epoch  == undefined) {
+        return undefined
+    }
+    var diff_ms =  epoch * 1000 * 30 // block time in seconds
+    var date = diff_ms + new Date('2020-08-24T22:00Z').getTime() // filecoin network start time
+    var converted_date = new Date(date)
+    return converted_date.toISOString().substring(0, 10)
+  }
